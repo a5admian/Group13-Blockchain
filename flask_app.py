dbc1ed7405c7f4a6797bd2e4b0d0ed7d2b34f7c5
a5admian <114887569+a5admian@users.noreply.github.com>
2024-04-04 13:22:13 -0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -3,3 +3,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 import hashlib
 import datetime
+
+class Block:
+  def __init__(self, data, previous_hash):
+    self.timestamp = datetime.datetime.now()
+    self.data = data
+    self.previous_hash = previous_hash
+    self.hash = self.calculate_hash
+
+  def calculate_hash(self):
+    hash_data = str(self.timestamp) + str(self.data) + str(self.previous_hash)
+    return hashlib.sha256(hash_data.encode()).hexidigest()
