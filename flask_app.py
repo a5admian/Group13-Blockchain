2b16b0a7fbf2fd3354747d50a6120daa1ed3d3ad
a5admian <114887569+a5admian@users.noreply.github.com>
2024-04-05 13:26:59 -0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -14,3 +14,20 @@ class Block:
   def calculate_hash(self):
     hash_data = str(self.timestamp) + str(self.data) + str(self.previous_hash)
     return hashlib.sha256(hash_data.encode()).hexidigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block("Genesis Block", "0")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+blockchain = Blockchain()
